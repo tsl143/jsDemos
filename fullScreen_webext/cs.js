a9ec2d0f03fa642b234c0a8148fc6109c08d08d2
Trishul <trishul.goel@gmail.com>
2018-12-24 14:37:22 +0100

    full screen version 2.0

--- fullScreen_webext/cs.js
+++ fullScreen_webext/cs.js
@@ -1,9 +1,8 @@
 const allowFullScreen = event => {
-	event.preventDefault(); event.stopPropagation();
-  	event = event || window.event;
-    if(event.ctrlKey && event.keyCode==70 && event.shiftKey) {
+    event.preventDefault();
+    event = event || window.event;
+    if (event.ctrlKey && event.keyCode == 70 && event.shiftKey) {
         document.documentElement.mozRequestFullScreen();
     }
 }
-
 document.addEventListener('keyup', allowFullScreen)
