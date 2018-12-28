f2f6a36bed6838c6f1850211d9669da322f49bc8
Trishul <trishul.goel@gmail.com>
2018-12-28 12:17:55 +0100

    fullscreen for chrome

--- fullScreen_webext/cs.js
+++ fullScreen_webext/cs.js
@@ -2,7 +2,8 @@ const allowFullScreen = event => {
     event.preventDefault();
     event = event || window.event;
     if (event.ctrlKey && event.keyCode == 70 && event.shiftKey) {
-        document.documentElement.mozRequestFullScreen();
+        if(document.documentElement.webkitRequestFullScreen) document.documentElement.webkitRequestFullScreen();
+        else if(document.documentElement.mozRequestFullScreen) document.documentElement.mozRequestFullScreen();
     }
 }
 document.addEventListener('keyup', allowFullScreen)
