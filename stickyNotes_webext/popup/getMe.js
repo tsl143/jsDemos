a1efdfad839ac89a32e394040005ebe6516a1e28
alie4hunt <31023467+alie4hunt@users.noreply.github.com>
2020-07-14 01:17:03 +0800

    created Bold function and button

--- stickyNotes_webext/popup/getMe.js
+++ stickyNotes_webext/popup/getMe.js
@@ -1,20 +1,24 @@
-
 const notePad = document.getElementById('tslNotes');
 const cleanPad = document.getElementById('cleanPad');
 const layOver = document.getElementById('layOver');
 const yes = document.getElementById('yesPlz');
 const no = document.getElementById('nope');
-
+const boldButton = document.getElementById('bold-button');
 
 const gettingItem = browser.storage.local.get('tslStickyNotes');
 
+
 gettingItem.then((res) => {
-try{
-  if(res.tslStickyNotes){
-    notePad.value = res.tslStickyNotes;
-  }
-}catch(e){}
+    try{
+        if(res.tslStickyNotes){
+            notePad.value = res.tslStickyNotes;
+        }
+    }catch(e){}
 });
+
+$('#testing').click(function (e) {
+    console.log('e');
+})
 
 notePad.addEventListener('keyup',()=>{
     browser.storage.local.set({ tslStickyNotes: notePad.value });
@@ -32,4 +36,14 @@ yes.addEventListener('click',()=>{
     notePad.value = "";
     browser.storage.local.set({ tslStickyNotes: "" });
     layOver.className="hidden";
+}, false);
+
+boldButton.addEventListener('click', ()=>{
+    console.log("press");
+    // $('#tslNotes').css('font-weight', '700');
+    document.execCommand('bold', false, null);
+}, false);
+
+testing.addEventListener('click', ()=>{
+    console.log("press");
 }, false);
