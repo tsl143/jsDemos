8e4cc734726173ad4526bd6377d0bca75a36cfe7
dexson83 <dexson9683@gmail.com>
2020-07-14 19:16:28 +0800

    update italic

--- stickyNotes_webext/popup/getMe.js
+++ stickyNotes_webext/popup/getMe.js
@@ -1,9 +1,10 @@
 const notePad = document.getElementById('tslNotes');
-const cleanPad = document.getElementById('cleanPad');
 const layOver = document.getElementById('layOver');
 const yes = document.getElementById('yesPlz');
 const no = document.getElementById('nope');
 const boldButton = document.getElementById('bold-button');
+const italicButton = document.getElementById('italic-button');
+const underlineButton = document.getElementById('underline-button');
 
 const gettingItem = browser.storage.local.get('tslStickyNotes');
 
@@ -24,10 +25,6 @@ notePad.addEventListener('keyup',()=>{
     browser.storage.local.set({ tslStickyNotes: notePad.value });
 }, false);
 
-cleanPad.addEventListener('click',()=>{
-    layOver.className="";
-}, false);
-
 no.addEventListener('click',()=>{
     layOver.className="hidden";
 }, false);
@@ -42,6 +39,18 @@ boldButton.addEventListener('click', ()=>{
     console.log("press");
     // $('#tslNotes').css('font-weight', '700');
     document.execCommand('bold', false, null);
+}, false);
+
+italicButton.addEventListener('click', ()=>{
+    console.log("press");
+    // $('#tslNotes').css('font-style', 'italic');
+    document.execCommand('italic', false, null);
+}, false);
+
+underlineButton.addEventListener('click', ()=>{
+    console.log("press");
+    // $('#tslNotes').css('text-Decoration', 'underline');
+    document.execCommand('underline', false, null);
 }, false);
 
 testing.addEventListener('click', ()=>{
