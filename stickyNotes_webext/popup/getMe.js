1ed07e0e877a345360a61e8e497fd7924b265aa7
alie4hunt <31023467+alie4hunt@users.noreply.github.com>
2020-07-16 22:45:56 +0800

    clear button

--- stickyNotes_webext/popup/getMe.js
+++ stickyNotes_webext/popup/getMe.js
@@ -2,6 +2,9 @@ const notePad = document.getElementById('tslNotes');
 const boldButton = document.getElementById('bold-button');
 const italicButton = document.getElementById('italic-button');
 const underlineButton = document.getElementById('underline-button');
+const cleanPad = document.getElementById('cleanPad');
+const no = document.getElementById('nope');
+const yes = document.getElementById('yesPlz');
 
 const gettingItem = browser.storage.local.get('tslStickyNotes');
 
@@ -13,6 +16,20 @@ gettingItem.then((res) => {
         }
     }catch(e){}
 });
+
+cleanPad.addEventListener('click',()=>{
+    layOver.className="";
+}, false);
+
+no.addEventListener('click',()=>{
+    layOver.className="hidden";
+}, false);
+
+yes.addEventListener('click',()=>{
+    notePad.innerHTML = "";
+    browser.storage.local.set({ tslStickyNotes: "" });
+    layOver.className="hidden";
+}, false);
 
 // document.getElementById('tslNotes').innerHTML = localStorage['tslNotes'] || 'type something...';
 // setInterval(function() {
